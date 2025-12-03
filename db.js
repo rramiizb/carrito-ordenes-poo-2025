a77b2ebbd5ed5360657289b389890e0acebc24b6
Ramiro <ramiro@example.com>
2025-12-02 22:23:09 -0300

    Cambios hechos desde el servidor

--- db.js
+++ db.js
@@ -1,6 +1,4 @@
 const mysql = require("mysql2/promise");
-
-<<<<<<< Updated upstream
 const db = mysql.createPool({
     host: "localhost",
     user: "grupo5",
--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -11,7 +11,7 @@ async function crearCarrito(req, res) {
     const { usuarioId } = req.body;
     if (!usuarioId) return res.status(422).json({ error: "FaltanCampos", message: "usuarioId requerido" });
 
-  await pool.query(
+  const [result] = await db.query(
     "INSERT INTO carts (id_usuario, estado) VALUES (?, 'activo')",
     [1]
 );
